ec969aee20e021b3b172b3b7161bf3a08abe0684
IT22371904SLIIT <kavindu2002tcc@gmail.com>
2026-01-16 16:18:28 +0530

    tailwind css

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -1,14 +1,47 @@
-import logo from './logo.png'
+import logo from './dmax logo.jpeg'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
+import about_img from './about_img.png'
+import bin_icon from './bin_icon.png'
+import cart_icon from './cart_icon.png'
+import contact_img from './contact_img.png'
+import cross_icon from './cross_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import exchange_icon from './exchange_icon.png'
+import menu_icon from './menu_icon.png'
+import profile_icon from './profile_icon.png'
+import quality_icon from './quality_icon.png'
+import razorpay_logo from './razorpay_logo.png'
+import search_icon from './search_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+import star_icon from './star_icon.png'
+import stripe_logo from './stripe_logo.png'
+import support_img from './support_img.png'
+import dmax_logo from './dmax_logo.jpeg'
 
 export const assets = {
     logo,
+    dmax_logo,
     add_icon,
     order_icon,
     upload_area,
-    parcel_icon
+    parcel_icon,
+    about_img,
+    bin_icon,
+    cart_icon,
+    contact_img,
+    cross_icon,
+    dropdown_icon,
+    exchange_icon,
+    menu_icon,
+    profile_icon,
+    quality_icon,
+    razorpay_logo,
+    search_icon,
+    star_dull_icon,
+    star_icon,
+    stripe_logo,
+    support_img
 }
-
